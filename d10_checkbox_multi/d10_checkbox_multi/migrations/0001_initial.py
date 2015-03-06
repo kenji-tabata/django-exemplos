cd9da917ad8e85fa26361c664ba24ed58daa47c4
Leandro Kenji <locky.kenji@gmail.com>
2015-03-06 17:14:04 -0300

    App 10: Como armazenar multiplos registros das checkbox

---
+++ d10_checkbox_multi/d10_checkbox_multi/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Check',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('nome', models.CharField(max_length=200)),
+                ('alternativa', models.CharField(max_length=256)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
