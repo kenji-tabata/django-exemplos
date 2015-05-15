002cd6b0ee89e358a7bfc6109e130546062ee298
Leandro Kenji <locky.kenji@gmail.com>
2015-05-15 11:02:32 -0300

    add customizando/login_class

---
+++ customizando/login_class/login_class/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from login_class.models import Clientes
+
+'''class AuthUserAclDec(models.Model):
+    usuario = models.ForeignKey('auth.User')
+    cliente = models.ForeignKey(Clientes)
+    
+    
+class AuthUserAclSis(models.Model):
+    usuario = models.ForeignKey('auth.User')
+    cliente = models.ForeignKey(Clientes)
+    
+class AuthUserClientes(models.Model):
+    usuario = models.ForeignKey('auth.User')
+    cliente = models.ForeignKey(Clientes)'''
+    
---
+++ customizando/login_class/login_class/usuario.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import authenticate, login
+
+class Usuario(object):
+
+    def auth_user(self, request, username, password):    
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                self.set_user_session(request, user)
+                login(request, user)
+                return True
+            else:
+                status = "O usuário não está ativo."
+
+        else:
+            status = "O nome de usuário ou senha está incorreto."
+        return status
+
+    def set_user_session(self, request, user_data):
+        request.session['nome_usuario'] = user_data.first_name
+        request.session['usuario'] = user_data.username
+
+    def get_user_session(self, request, dados):
+        context = {}
+
+        for list in dados:
+            if request.session.__contains__(list):
+                context[list] = request.session[list]
+            else:
+                context[list] = ""
+
+        return context
