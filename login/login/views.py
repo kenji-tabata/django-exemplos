3331cfe6722f646fc2db0fd7d63b3dda8a10e0b7
Leandro Kenji <locky.kenji@gmail.com>
2015-06-16 09:14:03 -0300

    Removendo exemplo login, login_session e login_class

--- login/login/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-def index(request):
-    return render(request, 'login/index.html', {})
-
-@login_required(login_url='/')
-def logado(request):
-    return render(request, 'login/logado.html', {})
