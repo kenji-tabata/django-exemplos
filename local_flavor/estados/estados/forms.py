286f531065fd52167f5d855bacd1727206c2d606
Leandro Kenji <locky.kenji@gmail.com>
2015-07-14 14:32:13 -0300

    Delete localflavor

--- local_flavor/estados/estados/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from local_flavor_estados.models import Combobox
-from localflavor.br.br_states import STATE_CHOICES
-
-class ComboboxForm(forms.ModelForm):
-    estado = forms.ChoiceField(choices=STATE_CHOICES)
-    class Meta:
-        model = Combobox
-        fields = ('estado',)
