6670659edf75a67f4c90b06b57f85267cffee802
Flávio Micheletti <flaviomicheletti@hotmail.com>
2015-07-15 10:42:50 -0300

    del experimentando/

--- experimentando/testando_view/testando_view/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from testando_view.models import Contato
-
-class ContatoForm(forms.ModelForm):
-    class Meta:
-        model = Contato
-        fields = ('nome','email','data','mensagem',)
-        
