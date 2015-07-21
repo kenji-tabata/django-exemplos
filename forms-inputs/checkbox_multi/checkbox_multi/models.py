2930a855a5a2e224dc5482696338ee4e83a8e550
Leandro Kenji <locky.kenji@gmail.com>
2015-07-21 14:58:01 -0300

    delete forms-inputs/checkboxs

--- forms-inputs/checkbox_multi/checkbox_multi/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Check(models.Model):
-    nome = models.CharField(max_length=200)
-    alternativa = models.CharField(max_length=256)
-    
-    def __str__(self):
-        return self.titulo
